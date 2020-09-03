b5c4288447c6d1638def180d7820d482d31581a7
DuongKien <ddk0705@gmail.com>
2020-09-03 16:53:30 +0700

    Handle type of params

--- src/utils/my-array.ts
+++ src/utils/my-array.ts
@@ -2,20 +2,24 @@ export class MyArray {
 
   private collection: (string | number)[] = [];
 
-  public add(value: any): void {
-    if (typeof value === 'string' || typeof value === 'number') {
-      if (this.collection.length) {
-        const valueType = typeof this.collection[0];
-        if (typeof value !== valueType) {
-          throw new Error(`Your value must be ${valueType}`);
-        } else {
-          this.collection.push(value);
-        }
+  public add(value?: any): void {
+    if (!value
+      || value === undefined
+      || value === null
+      || (typeof value !== 'string' && typeof value !== 'number')
+    ) {
+      throw new Error('Your value must be string or number');
+    }
+
+    if (this.collection.length) {
+      const valueType = typeof this.collection[0];
+      if (typeof value !== valueType) {
+        throw new Error(`Your value must be ${valueType}`);
       } else {
         this.collection.push(value);
       }
     } else {
-      throw new Error('Your value must be string or number');
+      this.collection.push(value);
     }
   }
 
